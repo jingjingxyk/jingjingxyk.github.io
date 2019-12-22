a3e772d7d6b7e81d23746403efc1555e604829ef
jingjingxyk <zonghengbaihe521@qq.com>
2019-12-22 20:07:54 +0800

    修改构建文件

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -2,14 +2,14 @@
 
 TIME=`date "+%Y%m%d%H%M"`
 git add .
-git commit -a -m "dockerfile file change ${TIME}"
-git push -u origin master
+git commit -a -m "file change ${TIME}"
+git push -u origin develop
 
 
 
-GIT_REVISION=`git log -1 --pretty=format:"%h"`
-version=${TIME}_${GIT_REVISION}
-tag_name=release-v${version}
-
-git tag -l ${tag_name}
-git push origin ${tag_name}
+#GIT_REVISION=`git log -1 --pretty=format:"%h"`
+#version=${TIME}_${GIT_REVISION}
+#tag_name=release-v${version}
+#
+#git tag -l ${tag_name}
+#git push origin ${tag_name}
