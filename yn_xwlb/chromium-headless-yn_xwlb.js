512570aac18bd161434ff475d62f94a292cb0b09
jingjingxyk <zonghengbaihe521@qq.com>
2022-04-06 01:29:14 +0800

    [2022-04-05T17:29:14Z] Auto update by script

--- yn_xwlb/chromium-headless-yn_xwlb.js
+++ yn_xwlb/chromium-headless-yn_xwlb.js
@@ -89,7 +89,7 @@ async function sleep(time) {
     console.log('==================')
     set.forEach((key,val)=>{
         console.log(key + ": " + val)
-        if(key === current || key=== yesterday) {
+        if(key === current || key === yesterday) {
             fs.writeFileSync(`yn_xwlb_content/${key}.json`, JSON.stringify(days_data[key]), function (error) {
                 if (error) {
                     console.log('写入失败')
