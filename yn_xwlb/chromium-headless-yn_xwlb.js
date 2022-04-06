971d9ead098b3f4720550d1e67388eba712a7b7c
jingjingxyk <zonghengbaihe521@qq.com>
2022-04-06 14:37:38 +0800

    [2022-04-06T06:37:38Z] Auto update by script

--- yn_xwlb/chromium-headless-yn_xwlb.js
+++ yn_xwlb/chromium-headless-yn_xwlb.js
@@ -25,15 +25,9 @@ async function sleep(time) {
 
     await page.evaluate(() => {
         document.querySelector("#page").click();
-        document.querySelector("#page").click();
-        document.querySelector("#page").click();
-        document.querySelector("#page").click();
-        document.querySelector("#page").click();
-        document.querySelector("#page").click();
-        document.querySelector("#page").click();
 
     })
-    await sleep(90)
+    await sleep(15)
     const res = await page.evaluate(() => {
         let res = Array.from(document.querySelectorAll("#liebiao .tuwen-con-box")).map((current_value) => {
             console.log(current_value)
@@ -98,7 +92,7 @@ async function sleep(time) {
     console.log('==================')
     set.forEach((key, val) => {
         console.log(key + ": " + val)
-        if (1 ||  key === current || key === yesterday) {
+        if (  key === current || key === yesterday) {
 
             fs.writeFileSync(__dirname + `/yn_xwlb_content/${key}.json`, JSON.stringify(days_data[key]), function (error) {
                 if (error) {
