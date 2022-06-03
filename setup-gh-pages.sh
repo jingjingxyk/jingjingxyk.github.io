4d3b2bc340ceb75f25ac50447e43ac1f80a0a936
jingjingxyk <zonghengbaihe521@qq.com>
2022-06-03 19:57:34 +0800

    add auto Thanks-Mirror.html

--- setup-gh-pages.sh
+++ setup-gh-pages.sh
@@ -13,14 +13,25 @@ cat /etc/os-release
 echo $(id -u)
 echo $(id -g)
 
-sudo apt install -y iputils-ping net-tools dnsutils iproute2 procps iputils-ping lsof
+sudo apt install -y iputils-ping net-tools dnsutils iproute2 procps iputils-ping lsof git python3 python3-pip
+
+git clone https://github.com/jingjingxyk/Thanks-Mirror.git --depth=1 --progress
+
+cd Thanks-Mirror
+bash build-local-doc.sh
+
+cd ${__DIR__}
+cp Thanks-Mirror/index.html ./Thanks-Mirror.html
 
 
 npm install
 mkdir -p gh-pages/yn_xwlb_content
 mkdir -p gh-pages/gh-pages
+
 cp -f index.html gh-pages/
 cp -f CNAME gh-pages
+cp -f ./Thanks-Mirror.html gh-pages
+
 npm run build
 git branch -a
 
