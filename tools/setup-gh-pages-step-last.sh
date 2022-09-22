79a95f7c053d25cd0c29f058de3c9683442f7a27
jingjingxyk <zonghengbaihe521@qq.com>
2022-09-22 14:38:53 +0800

    [2022-09-22T06:38:53Z] Auto update by script

--- tools/setup-gh-pages-step-last.sh
+++ tools/setup-gh-pages-step-last.sh
@@ -18,7 +18,10 @@ npm run build
 cp -f index.html gh-pages/
 cp -f CNAME gh-pages
 cp -f robots.txt gh-pages
+
 cp -rf vlogs gh-pages
+cp -rf scholar gh-pages
+
 cp -rf dist gh-pages/
 cp -rf ajax gh-pages/
 cp -rf note gh-pages/
