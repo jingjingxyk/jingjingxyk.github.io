57d88c9edc3925e54ad75ca9a39f7601c6e65d60
jingjingxyk <zonghengbaihe521@qq.com>
2022-09-30 16:56:49 +0800

    [2022-09-30T08:56:49Z] Auto update by script

--- tools/setup-gh-pages-step-02-chromium-extension.sh
+++ tools/setup-gh-pages-step-02-chromium-extension.sh
@@ -32,6 +32,8 @@ test -d extension-v3-test/.git ||  git clone https://github.com/jingjingxyk/exte
 cd extension-v3-test
 bash tools/release-archive.sh
 cp dist/extension-v3-test.zip ${__ROOT__}/gh-pages/chromium-extension/extension-v3-test.zip
+mkdir -p ${__ROOT__}/gh-pages/chromium-extension/extension-v3-test/rules/
+cp rules/*.json ${__ROOT__}/gh-pages/chromium-extension/extension-v3-test/rules/
 
 unset http_proxy
 unset https_proxy
