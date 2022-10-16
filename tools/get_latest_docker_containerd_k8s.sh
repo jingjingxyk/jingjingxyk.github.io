d023e375f406092a566c7f02729d374e42c30e56
jingjingxyk <zonghengbaihe521@qq.com>
2022-10-16 22:21:19 +0800

    [2022-10-16T14:21:19Z] Auto update by script

---
+++ tools/get_latest_docker_containerd_k8s.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -exu
+
+__DIR__=$(cd "$(dirname "$0")";pwd)
+cd ${__DIR__}
+
+__ROOT__=$(cd ${__DIR__}/../ ;pwd)
+
+cd ${__ROOT__}
+mkdir -p ${__ROOT__}/gh-pages/gh-pages
+
+
+curl -Lo ${__ROOT__}/gh-pages/gh-pages/dl.k8s.io.stable-1.txt https://dl.k8s.io/release/stable-1.txt
