69f7b5d9b9511f6732f6c48507ebd437a7d31a47
jingjingxyk <jingjingxyk@users.noreply.github.com>
2022-11-04 14:51:21 +0000

    deploy: 7bb0d9cb466097b96e9b2b41fc5c1631168203c7

---
+++ frontend-experimental-features/main.js
@@ -0,0 +1,63 @@
+window.performance.getEntries().map(x => {
+    //console.log(x)
+    console.log(x.entryType, x.name, x.type, x.initiatorType, x.nextHopProtocol)
+
+});
+
+// 选择需要观察变动的节点
+const targetNode = document.body;
+
+// 观察器的配置（需要观察什么变动）
+const config = {attributes: true, childList: true, subtree: true};
+
+// 当观察到变动时执行的回调函数
+const callback = function (mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            console.log('A child node has been added or removed.');
+        } else if (mutation.type === 'attributes') {
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
+
+// 创建一个观察器实例并传入回调函数
+const observer = new MutationObserver(callback);
+
+// 以上述配置开始观察目标节点
+observer.observe(targetNode, config);
+
+// 之后，可停止观察
+observer.disconnect();
+
+
+let script = document.createElement("script");
+script.setAttribute(
+    "src",
+    "https://www.jingjingxyk.com/ajax/libs/adapter-latest.js"
+);
+//script.setAttribute("type", "module");
+script.setAttribute("type", "application/javascript");
+script.setAttribute("charset", "utf-8");
+document.body.appendChild(script);
+
+(async () => {
+    await import ('https://www.xieyaokun.com/frontend-experimental-features/app.mjs');
+})();
+
+//浏览器控制台执行代码 ，检查效果
+//模块加载测试
+
+(() => {
+    let script = document.createElement("script");
+    script.setAttribute(
+        "src",
+        "https://www.xieyaokun.com/frontend-experimental-features/main.js"
+    ),
+        script.setAttribute("type", "application/javascript"),
+        script.setAttribute("charset", "utf-8"),
+        document.body.appendChild(script);
+})();
+
+
