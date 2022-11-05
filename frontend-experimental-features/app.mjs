222631d0be47d54ca98942437709e621f05c8928
jingjingxyk <jingjingxyk@users.noreply.github.com>
2022-11-05 15:33:58 +0000

    deploy: f536d83ee0b30e9050f5e39e0768fd4386067470

--- frontend-experimental-features/app.mjs
+++ frontend-experimental-features/app.mjs
@@ -1,5 +1,9 @@
 import * as THREE from  'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.146.0/three.module.min.js'
 import * as  utils from 'https://www.jingjingxyk.com/ajax/libs/jingjingxyk/frontend-utils/utils.js'
 
+import { JSONEditor } from "/third_party/josdejong/svelte-jsoneditor/main/index.js";
+
+
 const scene = new THREE.Scene();
 console.log(utils )
+console.log(utils.default )
