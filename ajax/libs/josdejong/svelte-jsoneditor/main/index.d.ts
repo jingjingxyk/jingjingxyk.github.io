549f276529e6fdf89eeea86f048d032cd3914519
jingjingxyk <jingjingxyk@users.noreply.github.com>
2023-12-01 14:42:43 +0000

    deploy: c29bcd031d06668a5c8adf1f2ee0d7c27f088d07

--- ajax/libs/josdejong/svelte-jsoneditor/main/index.d.ts
+++ ajax/libs/josdejong/svelte-jsoneditor/main/index.d.ts
@@ -857,7 +857,7 @@ type Callback = () => void;
  * This is useful for example when opening a model on top of another modal:
  * you only want the top modal to close on Escape, and not the second modal.
  */
-declare function onEscape(element: Element, callback: Callback): {
+declare function onEscape(element: Element | null, callback: Callback): {
     destroy: () => void;
 };
 
